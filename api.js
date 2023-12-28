30ada2068e0a02c61d806d0de503cb1537478121
ayushgit12 <ayush2005raj@gmail.com>
2023-12-28 13:12:50 +0530

    know more added to 1st and 2nd rows, but 3rd and 4th row not working

--- api.js
+++ api.js
@@ -203,6 +203,7 @@ function openNav(movie) {
   
   /* Close when someone clicks on the "x" symbol inside the overlay */
   function closeNav() {
+
     document.getElementById("myNav").style.width = "0%";
   }
 
