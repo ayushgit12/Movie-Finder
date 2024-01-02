77942c979e1af966451b63d3c2d4ee4900cfaf4a
ayushgit12 <ayush2005raj@gmail.com>
2024-01-02 15:57:35 +0530

    added a new row, but no images available

--- api.js
+++ api.js
@@ -144,7 +144,7 @@ function showMovies(data) {
     main[0].innerHTML = ''
 
     data.forEach(movie => {
-        const { title, poster_path, vote_average, overview, id, origin_country } = movie
+        const { title, poster_path, vote_average, id} = movie
         const movieEl = document.createElement('div')
         movieEl.classList.add('movie-list-item')
         movieEl.innerHTML = `
@@ -170,11 +170,17 @@ function showMovies(data) {
 function openNav(movie) {
     let id = movie.id
     console.log(movie)
-    if(movie.origin_country)
+    if(movie.origin_country && movie.release_date)
         document.getElementById("overlay-content").innerHTML = `<div class="origin-country">Country of Origin: ${movie.origin_country}</div><h1 style="color: gray">OVERVIEW</h1><h4 style="color: rgb(178, 212, 109); opacity:">Released on : ${movie.release_date}</h4><div style="font-size:17px; color: gray; padding:35px 80px">${movie.overview}</div>`
     
+    else if(movie.origin_country)
+        document.getElementById("overlay-content").innerHTML = `<div class="origin-country">Country of Origin: ${movie.origin_country}</div><h1 style="color: gray">OVERVIEW</h1><div style="font-size:17px; color: gray; padding:35px 80px">${movie.overview}</div>`
+
+    else if(movie.release_date)
+        document.getElementById("overlay-content").innerHTML = `<h1 style="color: gray">OVERVIEW</h1><h4 style="color: rgb(178, 212, 109); opacity:">Released on : ${movie.release_date}</h4><div style="font-size:17px; color: gray; padding:35px 80px">${movie.overview}</div>`
+    
     else
-        document.getElementById("overlay-content").innerHTML = `<h1 style="color: gray">OVERVIEW</h1><h4 style="color: rgb(178, 212, 109)">Released on : ${movie.release_date}</h4><div style="font-size:17px; color: gray; padding:25px">${movie.overview}</div>`
+        document.getElementById("overlay-content").innerHTML = `<h1 style="color: gray">OVERVIEW</h1><div style="font-size:17px; color: gray; padding:25px">${movie.overview}</div>`
     
     fetch(BASE_URL+"/movie/"+id+'/videos?'+API_KEY).then(res=>res.json()).then((videoData)=>{
         if(videoData){
@@ -279,6 +285,8 @@ function showMovies3(data) {
 
 }
 
+
+
 getTV2(TV_Series_URL)
 function getTV2(url) {
     fetch(url).then(res => res.json()).then(data => {
@@ -314,6 +322,45 @@ function showTV2(data) {
 
 
 }
+
+
+
+
+getMovies4(BASE_URL + '/discover/movie?sort_by=popularity.desc/comedy&' + API_KEY);
+function getMovies4(url) {
+    fetch(url).then(res => res.json()).then(data => {
+        //  console.log(data.results);
+        showMovies4(data.results);
+    })
+}
+
+function showMovies4(data) {
+
+    main[4].innerHTML = ''
+
+    
+    data.forEach(movie => {
+        const { title, poster_path, vote_average, overview, id} = movie
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie-list-item')
+        movieEl.innerHTML = `
+        <img src="${IMAGE_URL + poster_path}" alt="" class="movie-list-item-img" style=" box-shadow: 12px 7px 7px rgb(16, 16, 16);">
+                            <span class="movie-list-item-title">${title} </span><span class="${getColor(vote_average)}">${vote_average}</span>
+                            <p class="movie-list-item-desc">${overview}</p>
+                            <button class="movie-list-item-button">WATCH</button>
+                            <button class="know-more" id=${id}>Know More</button>
+                            `
+        main[4].appendChild(movieEl)
+        // console.log(document.getElementById(id));
+        console.log(document.getElementById(id))
+        document.getElementById(id).addEventListener('click',()=>{
+            openNav(movie)
+        })
+        
+    })
+}
+
+
 
 
 function getColor(vote) {
